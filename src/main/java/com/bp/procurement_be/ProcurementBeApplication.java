8f5d73a96bc474423d1c0003077c97920c3e6e23
Namira Aulia <naamiraaulia@gmail.com>
2026-06-23 12:32:12 +0700

    feat: initial commit with spring boot boilerplate

---
+++ src/main/java/com/bp/procurement_be/ProcurementBeApplication.java
@@ -0,0 +1,13 @@
+package com.bp.procurement_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bp/procurement_be/ProcurementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bp.procurement_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
